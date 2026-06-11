bfa2595efefdd43bc1fa1ab9cd9f47f35b82263d
Zhuoqi Chen <1075040474@qq.com>
2026-06-11 14:08:49 -0400

    add 11_14&11_15

--- learn_python_11/11_4_1_custom.py
+++ learn_python_11/11_4_1_custom.py
@@ -20,14 +20,75 @@ class Fib(object):
         if self.a > 100000:
             raise StopIteration()
         return self.a
+    # 对于Fib却报错。原因是__getitem__()传入的参数可能是一个int，
+    # 也可能是一个切片对象slice，所以要做判断
     def __getitem__(self, n):
-        a,b = 1,1
-        for x in range(n):
-            a, b = b, a+b
-        return a
+        if isinstance(n, int):
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice):
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
 
-# for n in Fib():
-#     print(n)
+for n in Fib():
+    print(n)
 f = Fib()
 print(f[0])
 print(f[10])
+# print(list(range(100))[0:5])
+print(f[5:10])
+print(f[:10])
+# 但是没有对step参数作处理：
+print(f[:10:2])
+
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+    def __getattr__(self, attr):
+        if attr == 'score':
+            return 99
+        if attr == 'age':
+            return lambda: 25
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+        
+s = Student('Adam')
+print(s.score)
+print(s.age())
+# print(s.grade)
+class AdvancedChain(object):
+    def __init__(self, path=''):
+        self._path = path
+    def __getattr__(self, path):
+        return AdvancedChain('%s/%s' % (self._path, path))
+    def __call__(self, param):
+        return AdvancedChain('%s/%s' % (self._path, param))
+    def __str__(self):
+        return self._path
+    __repr__ = __str__
+print(AdvancedChain().user(12345).profile)
+
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+    def __call__(self):
+        print('My name is %s.' % self.name)
+
+s = Student('Adam')
+s()
+
+print(callable(Student('Adam')))
+print(callable(max))
+print(callable([1, 2, 3]))
+print(callable(None))
+print(callable('str'))
---
+++ learn_python_11/11_5_1_enum.py
@@ -0,0 +1,28 @@
+from enum import Enum, unique
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+# value属性则是自动赋给成员的int常量，默认从1开始计数
+print(Month(1))
+@unique
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+day1 = Weekday.Mon
+print(day1)
+print(Weekday.Tue)
+print(Weekday['Wed'])
+print(Weekday.Tue.value)
+print(day1 == Weekday.Mon)
+print(day1 == Weekday.Tue)
+print(Weekday(1))
+print(day1 == Weekday(1))
+for name, member in Weekday.__members__.items():
+    print(name, '=>', member)
